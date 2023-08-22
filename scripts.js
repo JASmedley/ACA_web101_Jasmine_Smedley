57b99fc1715d3c5e8c110bcf97ade84ae183859f
JASmedley <jasmine.a.smedley@gmail.com>
2023-08-22 00:02:33 -0500

    the really sad finishing touches

--- scripts.js
+++ scripts.js
@@ -4,3 +4,8 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+const ShopDropdownMenu = () => {
+  const menus = document.querySelector(".menus")
+  menus.classList.toggle("showMenu")
+}
